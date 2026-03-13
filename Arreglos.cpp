8555182f67a59fbc5bfbdd7ef92daf9f71fa8d5a
Jcaceresc4 <jcaceresc4@miumg.edu.gt>
2026-03-12 19:41:00 -0600

    se agregaron arreglos

---
+++ Arreglos.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+main(){
+	
+	
+int datos[] = {10,40,100,250,5000};
+ for (int i : datos){
+ 	cout<<i<<endl;
+ }
+
+/*
+for (int i=0;i<tam;i++){
+	cout<<datos[i]<<endl;
+}	
+*/
+
+
+	system ("pause");
+}
